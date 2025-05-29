16bf72895a41e4b4cf9b864fa47606f51a15caaa
sitfuturz <sarimitfuturz@gmail.com>
2025-05-29 16:42:24 +0530

    add

--- src/env/env.local.ts
+++ src/env/env.local.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseURL: 'https://lng.itfuturz.in', // Update with your actual base URL
+  baseURL: 'http://localhost:2027', // Update with your actual base URL
   route: 'admin',
  
-  imageUrl:'https://lng.itfuturz.in',
+  imageUrl:'http://localhost:2027',
 };
